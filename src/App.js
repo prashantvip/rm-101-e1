79371a8c5a283ecbb2fe0563b861186296b92693
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-06-08 20:59:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import TaskApp from "./components/TaskApp.jsx"
+import task from "./data/tasks.json"
+import "./App.css"
 function App() {
-  return <div>{/* Code Here */}</div>;
+  return <div>
+    <TaskApp task = {task}></TaskApp>
+  </div>;
 }
 
 export default App;
